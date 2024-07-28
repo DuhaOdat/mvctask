5c8ab42a2379899bae9e4ca9b8190d3f6769faea
DuhaOdat <odatduha@gmail.com>
2024-07-28 17:49:14 +0300

    CRUD 28/7/2024

---
+++ MvcUserDatabaseFirst/MvcUserDatabaseFirst/Controllers/UserController.cs
@@ -0,0 +1,97 @@
+﻿using MvcUserDatabaseFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcUserDatabaseFirst.Controllers
+{
+    public class UserController : Controller
+    {
+        UsersInfoEntities db = new UsersInfoEntities();
+        // GET: User
+        public ActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+        //Get
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        //GET Edit
+        public ActionResult Edit(int id = 0)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //GET Details
+        public ActionResult Details(int id)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+    }
+}
---
+++ MvcUserDatabaseFirst/MvcUserDatabaseFirst/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcUserDatabaseFirst.MvcApplication" Language="C#" %>
