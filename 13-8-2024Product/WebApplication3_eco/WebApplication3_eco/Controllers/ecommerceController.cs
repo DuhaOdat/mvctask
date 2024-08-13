f83cd1956879557e18010f33c0f57f69a3baa92f
DuhaOdat <odatduha@gmail.com>
2024-08-13 23:10:46 +0300

    task Product 13-8-2024

---
+++ 13-8-2024Product/WebApplication3_eco/WebApplication3_eco/Controllers/ecommerceController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace WebApplication3_eco.Controllers
+{
+    public class ecommerceController : Controller
+    {
+        // GET: ecommerce
+        public ActionResult shop()
+        {
+            var products = Session["products"] as List<Product> ?? new List<Product>();
+            return View(products);
+        }
+
+        // GET: ecommerce/Create
+        public ActionResult addProduct()
+        {
+            return View();
+        }
+
+        // POST: ecommerce/Create
+        [HttpPost]
+        public ActionResult addProduct(string img, string productName, string price)
+        {
+            var products = Session["products"] as List<Product> ?? new List<Product>();
+
+            
+            products.Add(new Product
+            {
+                ImageUrl = img,
+                Name = productName,
+                Price = price
+            });
+
+            
+            Session["products"] = products;
+
+            return RedirectToAction("shop");
+        }
+    }
+
+    public class Product
+    {
+        public string ImageUrl { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+    }
+}
---
+++ 13-8-2024Product/WebApplication3_eco/WebApplication3_eco/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication3_eco.MvcApplication" Language="C#" %>
